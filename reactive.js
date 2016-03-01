e74676af1ebadee6881de3f08669f76782dced64
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2016-03-01 21:02:23 +0000

    deploy

--- reactive.js
+++ reactive.js
@@ -2,35 +2,78 @@
 
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-var w = document.documentElement.clientWidth * .995;
-var h = document.documentElement.clientHeight * .995;
+var _keyMapping;
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var w = document.documentElement.clientWidth * .98;
+var h = document.documentElement.clientHeight * .98;
+
+var keyMapping = (_keyMapping = {}, _defineProperty(_keyMapping, 38, 'DOWN'), _defineProperty(_keyMapping, 40, 'UP'), _keyMapping);
+
+var keyStream = Rx.Observable.fromEvent($(document), 'keydown').map(function (_ref) {
+  var keyCode = _ref.keyCode;
+  return keyCode;
+}).map(function (keyCode) {
+  return keyMapping[keyCode] ? keyMapping[keyCode] : keyCode;
+});
+var delayStream = Rx.Observable.interval(40).timeInterval().map(function (_ref2) {
+  var interval = _ref2.interval;
+  return interval;
+}).map(function (t) {
+  return t - 40;
+}).map(function (t) {
+  return t < 0 ? 0 : t;
+});
 var mouseMoveStream = Rx.Observable.fromEvent($('html'), 'mousemove');
-var mouseClickStream = Rx.Observable.fromEvent($('html'), 'click');
+var mouseClickStream = Rx.Observable.fromEvent($('html'), 'click').withLatestFrom(delayStream).filter(function (_ref3) {
+  var _ref4 = _slicedToArray(_ref3, 2);
 
-var treePropsStream = mouseClickStream.scan(function (tree) {
+  var event = _ref4[0];
+  var delay = _ref4[1];
+  return delay === 0;
+}).map(function (_ref5) {
+  var _ref6 = _slicedToArray(_ref5, 1);
+
+  var event = _ref6[0];
+  return event;
+});
+
+var powStream = keyStream.filter(function (keyCode) {
+  return keyCode === 'DOWN' || keyCode === 'UP';
+}).startWith('UP').map(function (keyName) {
+  return keyName === 'UP' ? 0.1 : -0.1;
+}).scan(function (pow, powDiff) {
+  var newPow = pow + powDiff;
+  return newPow < .1 ? .1 : newPow;
+}, 1);
+
+var treePropsStream = mouseClickStream.startWith('click').scan(function (tree) {
   return treeCreator.grow(tree);
 }, treeCreator.initNode());
 
-var angleStream = mouseMoveStream.map(function (_ref) {
-  var clientX = _ref.clientX;
-  var clientY = _ref.clientY;
+var angleStream = mouseMoveStream.map(function (_ref7) {
+  var clientX = _ref7.clientX;
+  var clientY = _ref7.clientY;
   return {
-    theta1: Math.PI * clientX / w - Math.PI / 2,
-    theta2: Math.PI * clientY / h - Math.PI / 2
+    theta1: 2 * Math.PI * clientX / w - Math.PI,
+    theta2: 2 * Math.PI * clientY / h - Math.PI
   };
-});
+}).startWith({ theta1: 0, theta2: 0 });
 
-var finalStream = Rx.Observable.combineLatest(treePropsStream, angleStream);
+var finalStream = Rx.Observable.combineLatest(treePropsStream, angleStream, powStream, delayStream);
 
-finalStream.subscribe(function (_ref2) {
-  var _ref3 = _slicedToArray(_ref2, 2);
+finalStream.subscribe(function (_ref8) {
+  var _ref9 = _slicedToArray(_ref8, 4);
 
-  var tree = _ref3[0];
-  var _ref3$ = _ref3[1];
-  var theta1 = _ref3$.theta1;
-  var theta2 = _ref3$.theta2;
-  return treeCreator.draw(tree, theta1, theta2);
+  var tree = _ref9[0];
+  var _ref9$ = _ref9[1];
+  var theta1 = _ref9$.theta1;
+  var theta2 = _ref9$.theta2;
+  var powStream = _ref9[2];
+  var delay = _ref9[3];
+
+  treeCreator.draw(tree, theta1, theta2, powStream, delay);
 }, function (err) {
   return console.log('Error: ' + err);
 }, function () {
--- tree.js
+++ tree.js
@@ -1,8 +1,14 @@
 'use strict';
 
 var treeCreator = function () {
-  var w = document.documentElement.clientWidth * .995;
-  var h = document.documentElement.clientHeight * .995;
+  var w = $(window).width() * .995;
+  var h = $(window).height() * .995;
+
+  var getId = function (id) {
+    return function () {
+      return id++;
+    };
+  }(0);
 
   var flatten1 = function flatten1(arrs) {
     return arrs.reduce(function (flatArr, arr) {
@@ -10,23 +16,25 @@ var treeCreator = function () {
     }, []);
   };
 
-  var createNodes = function createNodes(nodePropsTree, theta1, theta2, lastNode, i) {
+  var createNodes = function createNodes(nodePropsTree, theta1, theta2, pow, lastNode, i) {
     var dir = nodePropsTree.dir;
     var dTheta = !dir ? 0 : dir === 'left' ? theta1 : theta2;
-    var len = 1 / Math.sqrt((i - 2) * (i - 2) + 1);
+    var len = 1.5 / Math.pow(i + 1, pow);
     var theta = lastNode.theta + dTheta;
     var x1 = lastNode.x2;
     var y1 = lastNode.y2;
     var x2 = x1 + 2 * nodePropsTree.r * Math.cos(theta) * len;
     var y2 = y1 + 2 * nodePropsTree.r * Math.sin(theta) * len;
-    var node = { theta: theta, x1: x1, y1: y1, x2: x2, y2: y2 };
+    var id = nodePropsTree.id;
+    var node = { theta: theta, x1: x1, y1: y1, x2: x2, y2: y2, id: id };
     var children = nodePropsTree.children;
     return [node].concat(children.length ? flatten1(children.map(function (nodePropsTree) {
-      return createNodes(nodePropsTree, theta1, theta2, node, i + 1);
+      return createNodes(nodePropsTree, theta1, theta2, pow, node, i + 1);
     })) : []); // concating may be to slow (use immutable.js?)
   };
 
   var testNodes = {
+    id: getId(),
     theta: 0,
     r: 1,
     children: []
@@ -40,10 +48,12 @@ var treeCreator = function () {
       });
     } else {
       tree.children = [{
+        id: getId(),
         r: 1,
         dir: 'left',
         children: []
       }, {
+        id: getId(),
         dir: 'right',
         r: 1,
         children: []
@@ -69,10 +79,14 @@ var treeCreator = function () {
   });
 
   return {
-    draw: function draw(tree, theta1, theta2) {
-      var links = createNodes(tree, theta1, theta2, { x2: 3, y2: 0, theta: 0 }, 0);
+    draw: function draw(tree, theta1, theta2, pow, delay) {
+      var links = createNodes(tree, theta1, theta2, pow, { x2: 3, y2: 0, theta: 0 }, 0);
 
-      var selection = svg.selectAll('.branch').data(links).attr('d', diagonal);
+      var selection = svg.selectAll('.branch').data(links, function (d) {
+        return d.id;
+      }); //.attr('d', diagonal)
+
+      selection.transition().duration(40 + delay * 3).ease("linear").attr('d', diagonal);
 
       selection.enter().append('path').attr("stroke-width", 2).attr("fill", 'none').attr('class', 'branch').attr("stroke", "black").attr('d', diagonal);
     },
